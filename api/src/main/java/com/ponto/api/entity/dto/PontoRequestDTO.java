a00e84f248efad227746cdedc25b092aed224924
SotãoCerebral <130693907+SotaoCerebral@users.noreply.github.com>
2025-06-20 10:50:20 -0300

    Delete api directory

--- api/src/main/java/com/ponto/api/entity/dto/PontoRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.ponto.api.entity.dto;
-
-import com.ponto.api.entity.enums.Entradas;
-
-public record PontoRequestDTO(String descricao, Long UsuarioId, Entradas entradas) {
-}
--- api/src/main/java/com/ponto/api/entity/enums/Entradas.java
+++
@@ -1,6 +0,0 @@
-package com.ponto.api.entity.enums;
-
-public enum Entradas {
-    ENTRADA,
-    SAIDA
-}
